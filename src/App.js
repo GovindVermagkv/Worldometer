639314119d4c449c37f7585567d65fef76dcf78a
GovindVermagkv <sonygovind.gkv740@gmail.com>
2022-11-12 14:37:07 +0530

    start

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+import Example from './Components/trial';
+
+function App() {
+  return (
+    <div>
+    <h1>hello</h1>
+     <Example />
+    </div>
+  );
+}
+
+export default App;
